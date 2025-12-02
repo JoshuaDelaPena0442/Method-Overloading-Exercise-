04973f3edf811b3e7f876731d422c4ff7c5d4b77
Joshua Dela Pena <shiroganebigboi@gmail.com>
2025-12-02 13:22:06 -0600

    Completed exercises listed

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,37 @@
 {
     public class Program
     {
+
+        public static int Add(int number1, int number2)
+
+        {
+            return number1 + number2;
+        }
+
+        public static decimal Add(decimal numberOne, decimal numberTwo)
+
+        {
+            return numberOne + numberTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isMoney)
+        {
+            var sum = numOne + numTwo;
+
+            if (isMoney && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            
+            else if (isMoney && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else return sum.ToString();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 1, false));
         }
     }
 }
